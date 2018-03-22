6916da983459fd5bc944ad0ce7d3f49e4ad6a220
cdrbbt <iosdev@ety-idev-mba-03.local>
2018-03-22 12:22:14 +0200

    FoodTracker image view added

---
+++ httpTest/httpTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  httpTest
+//
+//  Created by iosdev on 22.3.2018.
+//  Copyright © 2018 cdrbbt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
