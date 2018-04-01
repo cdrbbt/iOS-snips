10934a41153ca4af88abd5b323c8624a1e6971c1
cdrbbt <iosdev@ety-idev-mba-03.local>
2018-04-01 19:38:31 +0300

    foodtracker: added rating controls

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +25,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         // Hide the keyboard.
         textField.resignFirstResponder()
         return true
-    }
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
     }
     
     //MARK: UIImagePickerControllerDelegate
