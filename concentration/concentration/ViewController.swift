59d65432433d6c22cb30e9a58a028c552d3c6d47
cdrbbt <iosdev@ety-idev-mba-03.local>
2018-04-18 12:29:23 +0300

    concentration:basic cards and flip

--- concentration/concentration/ViewController.swift
+++ concentration/concentration/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLable: UILabel!
+    
+    var flipCount = 0 {
+        didSet {
+           flipCountLable.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoice = ["👻","🎃","👻", "🎃"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +32,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            print("card numer = \(cardNumber)")
+            flipCard(withEmoji: emojiChoice[cardNumber], on: sender)
+        } else {
+            print("card missing")
+        }
+    }
+    
+    func flipCard(withEmoji emoji:String, on button: UIButton){
+        print("flipCard(withEmoji: \(emoji)")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
 
