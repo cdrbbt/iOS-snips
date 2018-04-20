ac28a88300434afae8424e8810176fbff72a93ba
cdrbbt <iosdev@ety-idev-mba-03.local>
2018-04-20 08:53:11 +0300

    concentration: model improved

---
+++ concentration/concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  concentration
+//
+//  Created by iosdev on 19.4.2018.
+//  Copyright © 2018 cdrbbt. All rights reserved.
+//
+
+import Foundation
---
+++ concentration/concentration/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  concentration
+//
+//  Created by iosdev on 19.4.2018.
+//  Copyright © 2018 cdrbbt. All rights reserved.
+//
+
+import Foundation
